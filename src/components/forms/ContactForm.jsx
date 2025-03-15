dc1c7c940d188a762b0d8a47cb14d0bf4fce7481
gi-t-ansari <tanzeelshamsh.in@gmail.com>
2025-03-16 04:01:46 +0530

    final commit

--- src/components/forms/ContactForm.jsx
+++ src/components/forms/ContactForm.jsx
@@ -1,19 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ContactForm.css";
 
 const ContactForm = () => {
-  const submitForm = () => {};
+  const [emailInputState, setEmailInputState] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  /**---------- FUNCTION TO VALIDATE EMAIL ------------ */
+  const validateEmail = (email) => /^[^@]+@[^@]+\.[^@]+$/.test(email);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    //Empty Form Submission is not allowed
+    if (!emailInputState.trim()) {
+      setShowError(true);
+      setEmailInputState("Enter email first.");
+      setTimeout(() => {
+        setEmailInputState("");
+        setShowError(false);
+      }, 2000);
+      return;
+    }
+
+    //Email Validation at Front-End
+    if (!validateEmail(emailInputState)) {
+      setShowError(true);
+      setEmailInputState("Invalid email format.");
+      setTimeout(() => {
+        setEmailInputState("");
+        setShowError(false);
+      }, 2000);
+      return;
+    }
+
+    setLoading(true);
+
+    fetch("https://test.ezworks.ai/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: emailInputState }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data?.success) {
+          setShowSuccess(true);
+          //The form is submitted successfully, API will return 200 in Response, and “ Form Submitted” will be displayed in the text field.
+          setEmailInputState("Form Submitted");
+          setTimeout(() => {
+            setEmailInputState("");
+            setShowSuccess(false);
+          }, 2000);
+        } else if (data?.detail) {
+          setShowError(true);
+          //An error will be returned in API Response when the email ends with @ez.works. Displaying that error inside the form field.
+          setEmailInputState(data?.detail);
+          setTimeout(() => {
+            setEmailInputState("");
+            setShowError(false);
+          }, 2000);
+        }
+      })
+      .catch(() => {
+        setShowError(true);
+        //Displaying the error inside error field if some other error occurred
+        setEmailInputState("Something went wrong. Please try again.");
+        setTimeout(() => {
+          setEmailInputState("");
+          setShowError(false);
+        }, 2000);
+      })
+      .finally(() => setLoading(false));
+  };
 
   return (
-    <form onSubmit={submitForm}>
-      <div className="input-container">
-        <input
-          className="email-input"
-          type="email"
-          placeholder={`Email Address`}
-        />
-        <button className="submit-button">Contact Me</button>
-      </div>
+    <form className="input-container" onSubmit={submitForm}>
+      <input
+        value={emailInputState}
+        onChange={(e) => setEmailInputState(e.target.value)}
+        className={`email-input`}
+        type="email"
+        placeholder={"Email Address"}
+        disabled={showSuccess || showError}
+      />
+      <button
+        className="submit-button"
+        disabled={loading || showSuccess || showError}
+      >
+        {loading ? "Contacting..." : "Contact Me"}
+      </button>
     </form>
   );
 };
